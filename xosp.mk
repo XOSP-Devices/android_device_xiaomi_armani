e23f0b437d5f39b2c962dd22feb179d543e9c9a0
Nitin chobhe <nitin1438@users.noreply.github.com>
2016-05-28 18:14:52 +0530

    XOSPify

--- xosp.mk
+++ xosp.mk
@@ -21,12 +21,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/armani/device.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
 
+# Inherit some common XOSP stuff
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
+RELEASE_TYPE := XOSP_OFFICIAL
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := xosp_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi
